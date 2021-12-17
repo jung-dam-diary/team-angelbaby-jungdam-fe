81ac21fedbf35c71ac908da9d3d7784ce1fe9472
minsgy <alstjrchl97@naver.com>
2021-12-17 14:39:25 +0900

    ♻️ refactor #141 : DefaultTemplate 반응형 페이지 구상 중

--- src/styles/DefaultTemplate.jsx
+++ src/styles/DefaultTemplate.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
+import { ReactComponent as Logo } from '@assets/Image/Logo.svg';
 import styled from '@emotion/styled';
+import font from '@assets/fonts';
 const MainWrapper = styled.div`
   display: flex;
   width: 100%;
@@ -9,6 +11,42 @@ const MainWrapper = styled.div`
   padding: 32px;
   overflow-y: auto;
 `;
+
+const ResponsiveWrapper = styled(MainWrapper)`
+  width: 414px;
+  height: 736px;
+  flex-direction: row;
+  justify-content: center;
+  padding: 0;
+`;
+
+const ResponsiveTemplate = styled.div`
+  width: 414px;
+  height: 736px;
+  border: 1px solid #c4c4c4;
+  border-radius: 4px;
+`;
+
+const Box = styled.div`
+  margin-right: 48px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 350px;
+  height: 100%;
+`;
+
+const LogoBox = styled.div`
+  text-align: center;
+`;
+
+const Title = styled.h1`
+  margin-top: 35px;
+  ${font.heading_20};
+`;
+
+const Content = styled.p``;
 
 const DefaultTemplate = ({ children }) => {
   const [width, setWidth] = useState(window.innerWidth);
@@ -26,7 +64,15 @@ const DefaultTemplate = ({ children }) => {
   return width < 456 ? (
     <MainWrapper>{children}</MainWrapper>
   ) : (
-    '태블릿/데스크탑은 지원하지 않습니다.'
+    <ResponsiveWrapper>
+      <Box>
+        <LogoBox>
+          <Logo />
+          <Title>특별한 순간의 일기로 추억에 빠져보세요.</Title>
+        </LogoBox>
+      </Box>
+      <ResponsiveTemplate>{children}</ResponsiveTemplate>
+    </ResponsiveWrapper>
   );
 };
 
